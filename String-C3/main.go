7f5a6fae35f95c6eee834bc4ae4c9ede4cda5d18
Wakar Seraj Khan <wakar473@gmail.com>
2024-11-26 22:50:04 +0530

    string

---
+++ String-C3/main.go
@@ -0,0 +1,12 @@
+package main
+import(
+)
+func main(){
+	println("Hello Wakar")
+	println ("Hey wakar"[1])
+	println("hello " + "wakar")
+}
+
+/*A space is also considered a character, so the
+string's length is 11 not 10 and the 3 rd line has
+"Hello " instead of "Hello" .*/

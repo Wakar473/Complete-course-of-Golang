e8001a417fc1a09edd8c3e7868266329ae074b9b
Wakar Seraj Khan <wakar473@gmail.com>
2024-11-28 16:58:00 +0530

    feat:added some basic concept in variable

--- Variable-C4/main.go
+++ Variable-C4/main.go
@@ -7,4 +7,20 @@ func main (){
 	var a string
 	a = "Apple"
 	println (a)
+
+
+
+	var x string
+	x = "first"
+	println(x)
+	x = "second"
+	println(x)
+
+
+	//infact you can even do this
+	var y string
+	y = "one"
+	println(y)
+	y = y + " two"
+	println(y)
 }

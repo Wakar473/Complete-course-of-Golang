e189971b7fa515c055807957693c620ae5c47f3e
Wakar Seraj Khan <wakar473@gmail.com>
2024-11-29 18:56:53 +0530

    chore:added few concepts in variable

--- Variable-C4/main.go
+++ Variable-C4/main.go
@@ -23,4 +23,31 @@ func main (){
 	println(y)
 	y = y + " two"
 	println(y)
+
+	var p string ="hey"
+	var q string = "hey"
+	println(p==q)
+
+
+	var m string ="hey"
+	var n string = "buddy"
+	println(m==n)
+
+/*Notice the : before the = and that no type was speci-
+fied. The type is not necessary because the Go compiler
+is able to infer the type based on the literal value you
+assign the variable. (Since you are assigning a string
+literal, x is given the type string ) The compiler can
+also do inference with the var statement:*/
+	 j := 5
+println (j)
+
+println(k)
+
 }
+var k string = "Hello World"
+/*Notice that I moved the variable outside of the main
+function. This means that other functions can access
+this variable: */
+
+/*we can use global variable any where in the code.*/ 

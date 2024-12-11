e8ddbfcdfe843b570682418e0966323302df0160
Wakar Seraj Khan <wakar473@gmail.com>
2024-12-11 20:43:00 +0530

    feat:Basic of map

---
+++ Array-Slices-MapsC-6/map/main.go
@@ -0,0 +1,19 @@
+package main
+import(
+	"fmt"
+)
+
+func main() {
+	var a map[string]int
+	a["key"] = 10
+	fmt.Println(a["key"])
+/*
+The map type is represented by the keyword map , fol-
+lowed by the key type in brackets and finally the value
+type. If you were to read this out loud you would say “ a
+is a map of string s to int s.”
+	*/
+
+
+
+}
